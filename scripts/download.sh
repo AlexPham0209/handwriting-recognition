6bdb778813b5754ff0b2aaa80c6f25ddf8ca693c
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-07-24 15:32:43 -0400

    Moved download script to Scripts folder

---
+++ scripts/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L -o --compressed data/handwriting-recognition.zip\
+  https://www.kaggle.com/api/v1/datasets/download/landlord/handwriting-recognition
+
+echo "Downloading Finished"
